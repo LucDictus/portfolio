939e852d773c617291240e87b035e837699def5c
OnsLucerd <lucdictus161@gmail.com>
2023-05-21 13:16:06 +0200

    navigation start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
+import Navigation from './components/Navigation/Navigation';
+
+export function navOpen() {
+  console.log('hoi');
+}
 
 function App() {
+
   return (
     <div className="App">
-      <HomePage />
+      {/* <div id='homepage'>
+        <HomePage />
+      </div> */}
+      <div id='navigation'>
+        <Navigation/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './Navigation.css';
+
+function Navigation() {
+    return ( 
+        <>
+            <div id='parent'>
+                <div id='container'>
+                    <div id='Navigation-projects-container'>
+                        <a class='Navigation-project'>
+                            <div className='Navigation-project-number-container'>
+                                <div className='Navigation-project-hashtag'>#</div>
+                                <div className='Navigation-project-number'>1</div>
+                            </div>
+                            <div className='Navigation-project-info'>
+                                <div className='Navigation-project-title'>Project Title</div>
+                                <div className='Navigation-project-text'>bedrijf/school, jaar</div>
+                            </div>
+                        </a>
+
+                        <a class='Navigation-project'>
+                            <div className='Navigation-project-number-container'>
+                                <div className='Navigation-project-hashtag'>#</div>
+                                <div className='Navigation-project-number'>2</div>
+                            </div>
+                            <div className='Navigation-project-info'>
+                                <div className='Navigation-project-title'>Project Title</div>
+                                <div className='Navigation-project-text'>bedrijf/school, jaar</div>
+                            </div>
+                        </a>
+
+                        <a class='Navigation-project'>
+                            <div className='Navigation-project-number-container'>
+                                <div className='Navigation-project-hashtag'>#</div>
+                                <div className='Navigation-project-number'>3</div>
+                            </div>
+                            <div className='Navigation-project-info'>
+                                <div className='Navigation-project-title'>Project Title</div>
+                                <div className='Navigation-project-text'>bedrijf/school, jaar</div>
+                            </div>
+                        </a>
+
+                        <a class='Navigation-project'>
+                            <div className='Navigation-project-number-container'>
+                                <div className='Navigation-project-hashtag'>#</div>
+                                <div className='Navigation-project-number'>4</div>
+                            </div>
+                            <div className='Navigation-project-info'>
+                                <div className='Navigation-project-title'>Project Title</div>
+                                <div className='Navigation-project-text'>bedrijf/school, jaar</div>
+                            </div>
+                        </a>
+
+                        <a class='Navigation-project'>
+                            <div className='Navigation-project-number-container'>
+                                <div className='Navigation-project-hashtag'>#</div>
+                                <div className='Navigation-project-number'>5</div>
+                            </div>
+                            <div className='Navigation-project-info'>
+                                <div className='Navigation-project-title'>Project Title</div>
+                                <div className='Navigation-project-text'>bedrijf/school, jaar</div>
+                            </div>
+                        </a>
+                    </div>
+                    <a>
+                        <div id='Navigation-name'>
+                            Luc Dictus <br></br>
+                            <span>Front-End Dev.</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navigation;
