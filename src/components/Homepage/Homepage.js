a3231e1d69fca5fc14409070d9df30d429225225
Luc Dictus <lucdictus161@gmail.com>
2024-01-20 20:20:48 +0100

    portfolio started

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,46 @@
+import './Homepage.css';
+import { gsap } from "gsap";
+
+function Homepage() {
+  return (
+    <div className="Homepage-container">
+        <div className="Homepage-background-glow"></div>
+
+        <div className="Homepage-title">
+            <h1>
+                Hé daar, ontdek mijn digitale creaties op mijn website. Benieuwd? Kom binnen en laat je inspireren!
+            </h1>
+        </div>
+
+        <div className="Homepage-projects-container">
+            <div className="Homepage-project-container" id="project-one-background">
+                <div className="Homepage-project-created">coded</div>
+                <div className="Homepage-project-title">Street Fighter II</div>
+            </div>
+
+            <div className="Homepage-project-container" id="project-two-background">
+                <div className="Homepage-project-created">designed & coded</div>
+                <div className="Homepage-project-title">Cinema App</div>
+            </div>
+
+            <div className="Homepage-project-container" id="project-three-background">
+                <div className="Homepage-project-created">coded</div>
+                <div className="Homepage-project-title">Video call</div>
+            </div>
+
+            <div className="Homepage-project-container" id="project-four-background">
+                <div className="Homepage-project-created">branding & designed & coded</div>
+                <div className="Homepage-project-title">Horloge app</div>
+            </div>
+        </div>
+
+        <div className="Homepage-footer">
+            <div className="Homepage-footer-title">
+                Wil je iets creëren of wil je meer weten? Neem contact met mij op.
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Homepage;
