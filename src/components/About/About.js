1909fda325b9b24f0e8726b90c45c5596380f176
LucDictus <lucdictus161@gmail.com>
2024-03-22 09:43:27 +0100

    about me page desktop

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -27,24 +27,25 @@ function About() {
             </div>
 
             <div className="About-content-skills-image-container">
-              <img src="./assets/images/AboutMeSkills-1.png" alt="Skills" className="About-content-skills-image-top" />
-              <img src="./assets/images/AboutMeSkills-2.png" alt="Skills" className="About-content-skills-image-top" />
-              <img src="./assets/images/AboutMeSkills-3.png" alt="Skills" className="About-content-skills-image-bottom" />
+              <img src="./assets/images/skills-image-1.jpg" alt="Skills" className="About-content-skills-image-top" id="about-skills-image-1" />
+              <img src="./assets/images/skills-image-2.jpg" alt="Skills" className="About-content-skills-image-top" id="about-skills-image-2" />
+              <img src="./assets/images/skills-image-3.png" alt="Skills" className="About-content-skills-image-bottom" id="about-skills-image-3" />
             </div>
           </div>
 
           <div className="About-content-about-me-container">
-            <div className="About-content-about-me-image-container">
-              <img src="./assets/images/AboutMeImage.png" alt="About me" className="About-content-about-me-image" />
-              <div className="About-content-about-me-image-name">Luc Dictus</div>
-            </div>
+            <div className="About-content-title">Over mij</div>
 
-            <div className="About-content-about-me-text-container">
-              <div className="About-content-about-me-title">Over mij</div>
+            <div className="About-content-about-me-content-container">
+              <div className="About-content-about-me-image">
+                <img src="./assets/images/about-me-image.jpg" alt="About me" className="About-content-about-me-image" />
+              </div>
+
               <div className="About-content-about-me-text">
                 Ik ben een creatieve front-end developer met een passie voor design en user experience. Ik ben gespecialiseerd in het ontwikkelen van websites en webapplicaties met HTML, CSS en JavaScript. Ik ben ook bekend met React, Node.js, Express en MongoDB. Ik ben altijd op zoek naar nieuwe technologieën en frameworks om mijn kennis uit te breiden.
               </div>
             </div>
+            
           </div>
 
           <div className="About-content-blog-container">
